7d053c4ee2b5c045196e3d160eddce3faf109211
uwaish55 <prashantgupta9169>
2020-12-13 12:05:10 +0530

    Updates

---
+++ precache-manifest.d019c07d7bd251a372896ef2435a46d8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409d78e17e2d8262e5f9e90d8ed21cc4",
+    "url": "/quizapp/index.html"
+  },
+  {
+    "revision": "07c1afc4bfacabd9cae6",
+    "url": "/quizapp/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ecc6d03862a43af5d927",
+    "url": "/quizapp/static/css/main.53b54255.chunk.css"
+  },
+  {
+    "revision": "07c1afc4bfacabd9cae6",
+    "url": "/quizapp/static/js/2.db5147b1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/quizapp/static/js/2.db5147b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00dbc3375193d885e5c",
+    "url": "/quizapp/static/js/3.2f80b6c9.chunk.js"
+  },
+  {
+    "revision": "196f31064c641ef86b22f34244b7ad03",
+    "url": "/quizapp/static/js/3.2f80b6c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc6d03862a43af5d927",
+    "url": "/quizapp/static/js/main.6b82020e.chunk.js"
+  },
+  {
+    "revision": "ce7672e1600a75e6a127",
+    "url": "/quizapp/static/js/runtime-main.c1d60c05.js"
+  },
+  {
+    "revision": "dfcdacd70fa97f883865459151bf89fc",
+    "url": "/quizapp/static/media/completed.dfcdacd7.mp3"
+  },
+  {
+    "revision": "ee6aadae18d9da389dc08dd8d72e3721",
+    "url": "/quizapp/static/media/correct-answer.ee6aadae.mp3"
+  },
+  {
+    "revision": "c7106542fc4e8075bf1bae4bf6854692",
+    "url": "/quizapp/static/media/wrong-answer.c7106542.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/uwaishkhan55/quizapp/precache-manifest.c181fc0a539400dc3b0d2c09d15b2fe5.js"
+  "/quizapp/precache-manifest.d019c07d7bd251a372896ef2435a46d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uwaishkhan55/quizapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quizapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
